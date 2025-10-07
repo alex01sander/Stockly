00afd269d745615e5b818c5640d24402ac8bede2
Alex Britto <alexsander01@hotmail.com.br>
2025-10-07 16:04:11 -0300

    feat: add products table

---
+++ app/products/_components/columns-table.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { Badge } from "@/app/_components/ui/badge";
+import { Product } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import {
+  CircleArrowUp,
+  CircleCheckIcon,
+  CircleChevronUpIcon,
+  CircleIcon,
+} from "lucide-react";
+
+const getStatusLabel = (status: string) => {
+  if (status === "IN_STOCK") {
+    return "Em estoque";
+  }
+  return "Fora de estoque";
+};
+
+export const productsTableColumns: ColumnDef<Product>[] = [
+  {
+    accessorKey: "name",
+    header: "Nome do produto",
+  },
+  {
+    accessorKey: "price",
+    header: "Valor unitário",
+  },
+  {
+    accessorKey: "stock",
+    header: "Estoque",
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => {
+      const product = row.original;
+      const label = getStatusLabel(product.status);
+      return (
+        <Badge
+          variant={label === "Em estoque" ? "default" : "outline"}
+          className="gap-2"
+        >
+          <CircleIcon
+            size={12}
+            className={
+              label === "Em estoque" ? "text-green-500" : "text-red-500"
+            }
+          />
+          {label}
+        </Badge>
+      );
+    },
+  },
+];
